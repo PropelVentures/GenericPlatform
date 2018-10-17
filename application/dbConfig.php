42615f6d69314b6a23e7532b2de204118e1e7ba2
dharmeshpjain <jain.dharmesh@gmail.com>
2018-10-18 02:32:57 +0530

    changes containing files and folder reorganization, new pagination stting, maintaining filter values and checklist settings

--- application/dbConfig.php
+++ application/dbConfig.php
@@ -2,15 +2,15 @@
 
 
 
-$config['db_name'] = "genericsandbox2";
+$config['db_name'] = "genericplatform";
 
-$config['db_user'] = "genericsandbox2";
+#$config['db_user'] = "genericinternal";
 
-$config['db_password'] = "Upwork0703!**";
+#$config['db_password'] = "Upwork0814!!";
 
-/* $config['db_user'] = "root";
+$config['db_user'] = "root";
 
-$config['db_password'] = ""; */
+$config['db_password'] = "";
 
 $config['db_host'] = "localhost";
 
