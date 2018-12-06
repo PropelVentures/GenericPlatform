ecc6782df2d1a223e8c29becaa458e67c4ac3f0e
ShailyA <shailyagarwal64@hotmail.com>
2018-12-06 10:30:52 +0530

    changed in system-config for all sandbox

--- application/system-config.php
+++ application/system-config.php
@@ -4,55 +4,50 @@
 
 define("SESSION_AUTO_TIMEOUT","9200");
 
-
 // =======================================================================
 // DATABASE CONFIG (Location, CREDENTIALS)
 // =======================================================================
-
-$config['db_host'] = "localhost";
-
-#$config['db_name'] = "genericplatform";
-#$config['db_user'] = "genericinternal";
-#$config['db_password'] = "Upwork0814!!";
-
-
-/*
-$config['db_name'] = "genericsandbox2";
-$config['db_user'] = "genericsandbox2";
-$config['db_password'] = "Upwork!081461!";
-*/
-
-
-$config['db_name'] = "genericplatform";
-$config['db_user'] = "genericinternal";
-$config['db_password'] = "Upwork0814!!";
-
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "generic";
+	$config['db_user'] = "root";
+	$config['db_password'] = "";
+} elseif($_SERVER['HTTP_HOST'] == 'genericsandbox1.cjcornell.net') {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "genericsandbox1";
+	$config['db_user'] = "genericsandbox1";
+	$config['db_password'] = "Lennin1009##";
+} elseif($_SERVER['HTTP_HOST'] == 'genericsandbox2.cjcornell.net') {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "genericsandbox2";
+	$config['db_user'] = "genericsandbox2";
+	$config['db_password'] = "Lennin1009##";
+} elseif($_SERVER['HTTP_HOST'] == 'genericsandbox3.cjcornell.net') {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "genericsandbox3";
+	$config['db_user'] = "genericsandbox3";
+	$config['db_password'] = "Lennin1009##";
+} elseif($_SERVER['HTTP_HOST'] == 'genericsandbox4.cjcornell.net') {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "genericsandbox4";
+	$config['db_user'] = "genericsandbox4";
+	$config['db_password'] = "Lennin1009##";
+} else {
+	$config['db_host'] = "localhost";
+	$config['db_name'] = "genericplatform";
+	$config['db_user'] = "genericinternal";
+	$config['db_password'] = "Lennin1009##";
+}
 
 $GLOBALS['db-host'] =  $config['db_host'];
 $GLOBALS['db-username'] = $config['db_name'] ;
 $GLOBALS['db-password'] =  $config['db_password'] ;
 $GLOBALS['db-database'] = $config['db_name'] ;
 
-
 if(!empty($config['db_name'])){
-
-    $_SESSION['config'] = $config;
+	$_SESSION['config'] = $config;
 }else{
-
-    unset($_SESSION['config']);
-}
-
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-    $GLOBALS['db-username'] = "root";
-    $GLOBALS['db-host'] = "localhost";
-    $GLOBALS['db-password'] = "";
-    $GLOBALS['db-database'] = "generic";
-} else {
-
-    $GLOBALS['db-host'] = $dbconfig->db_host;
-    $GLOBALS['db-username'] = $dbconfig->username;
-    $GLOBALS['db-password'] = $dbconfig->password;
-    $GLOBALS['db-database'] = $dbconfig->database;
+	unset($_SESSION['config']);
 }
 
 require_once("system-defines.php");
